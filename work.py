c3e65491b59063181694a95b6754fdcc46f8b6fe
avac886 <avac886@gmail.com>
2025-03-26 09:24:44 -0400

    ee

--- work.py
+++ work.py
@@ -1,3 +1,11 @@
 class flashcard():
-    def __init__(self, statement, answer):
-        
+    def __init__(self, subject, phrase, answer):
+        self.subject = subject
+        self.phrase = phrase
+        self.answer = answer
+    def display(self):
+        return f"{self.phrase}: {self.answer}"
+    def dict(self):
+        return {"subject": self.subject, "phrase": self.phrase, "answer": self.answer}
+
+new_flashcard = flashcard
