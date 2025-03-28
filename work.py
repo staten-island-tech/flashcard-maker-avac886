6a657c48ee7e7d015f81e288cfaf7944127aca67
avac886 <avac886@gmail.com>
2025-03-28 09:25:12 -0400

    card

--- work.py
+++ work.py
@@ -9,17 +9,19 @@ class flashcard():
     def dict(self):
         return {"phrase": self.phrase, "answer": self.answer}
 
+while True:
+    cards = []
+    new_phrase = input("Enter an English phrase/word: ")
+    new_answer = input("Enter the Russian translation of that phrase/word: ")
+    new_flashcard = flashcard(new_phrase, new_answer)
 
-cards = []
-new_phrase = input("Enter an English phrase/word: ")
-new_answer = input("Enter the Russian translation of that phrase/word: ")
-new_flashcard = flashcard(new_phrase, new_answer)
-cards_data = [flashcard.to_dict() for flashcard in cards]
-
-
-cards.append(new_flashcard)
-cards_data = [flashcard.dict() for flashcard in cards]
-with open('flashcards.json', 'w', encoding='utf-8') as json_file:
-    json.dump(cards_data, json_file, ensure_ascii=False, indent=4)
-
-
+    cards.append(new_flashcard)
+    cards_data = [flashcard.dict() for flashcard in cards]
+    with open('flashcards.json', 'w', encoding='utf-8') as json_file:
+        json.dump(cards_data, json_file, ensure_ascii=False, indent=4)
+    try:
+        with open("flashcards.json", "r") as file:
+            cards_data = json.load(file)
+    except FileNotFoundError:
+        cards_data = []
+    print(new_flashcard.display())
