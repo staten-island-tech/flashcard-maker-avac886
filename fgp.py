3829f80d0b6a79c7dd66c2008404ed460e79c043
avac886 <avac886@gmail.com>
2025-04-03 09:26:12 -0400

    asd

--- fgp.py
+++ fgp.py
@@ -13,28 +13,15 @@ cards = []
 new_phrase = input("Enter an English phrase/word: ")
 new_answer = input("Enter the Russian translation of that phrase/word: ")
 new_flashcard = flashcard(new_phrase, new_answer)
-print(new_flashcard.display())
-cards_data = [card.to_dict() for card in cards]
+
+cards_data = [flashcard.dict() for flashcard in cards]
 with open("cards.json", "w") as file:
     json.dump(cards_data, file, indent=4)
-    
-
-""" while True:
-    cards = []
-    new_phrase = input("Enter an English phrase/word: ")
-    new_answer = input("Enter the Russian translation of that phrase/word: ")
-    new_flashcard = flashcard(new_phrase, new_answer)
-    cards.append(new_flashcard)
-
-    cards_data = [flashcard.dict() for flashcard in cards]
-    with open('flashcards.json', 'w', encoding='utf-8') as json_file:
-        json.dump(cards_data, json_file, ensure_ascii=False, indent=4)
-    try:
-        with open("flashcards.json", "r") as file:
-            cards_data = json.load(file)
-    except FileNotFoundError:
-            cards_data = []
-    print(cards_data) """
-
-
-
+try:
+    with open("cards.json", "r") as file:
+        cards_data = json.load(file)
+except FileNotFoundError:
+    cards_data = []
+cards_data.append(new_flashcard.dict()) # dict not to_dict dummy
+with open("cards.json", "w") as file:
+    json.dump(cards_data, file, indent=4)
